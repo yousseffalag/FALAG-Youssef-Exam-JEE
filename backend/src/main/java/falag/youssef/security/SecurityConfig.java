270d3cb97cf98f923a1901f2e63a21e29f59bbb7
Ussefflg <falagyoussef@gmail.com>
2026-05-11 10:37:54 +0100

    feat(Securoty) : Implement Security Layer

---
+++ backend/src/main/java/falag/youssef/security/SecurityConfig.java
@@ -0,0 +1,132 @@
+package falag.youssef.security;
+
+
+import com.nimbusds.jose.jwk.source.ImmutableSecret;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.jose.jws.MacAlgorithm;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import javax.crypto.spec.SecretKeySpec;
+
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity(prePostEnabled = true)
+public class SecurityConfig {
+
+    @Value("${jwt.secretKey}")
+    private String secretKey;
+
+    @Bean
+    public PasswordEncoder passwordEncoder () {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public InMemoryUserDetailsManager inMemoryUserDetailsManager() {
+        return new InMemoryUserDetailsManager(
+                User.withUsername("admin")
+                        .password(passwordEncoder().encode("1234"))
+                        .authorities("ROLE_ADMIN")
+                        .build(),
+                User.withUsername("client")
+                        .password(passwordEncoder().encode("1234"))
+                        .authorities("ROLE_CLIENT")
+                        .build(),
+                User.withUsername("employe")
+                        .password(passwordEncoder().encode("1234"))
+                        .authorities("ROLE_EMPLOYE")
+                        .build()
+        );
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+
+        return httpSecurity
+                .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .csrf(AbstractHttpConfigurer::disable)
+                .cors(Customizer.withDefaults())
+                .authorizeHttpRequests(ar -> ar
+                        .requestMatchers("/auth/login/**").permitAll()
+                        .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
+                        .requestMatchers(
+                        "/v3/api-docs/**",
+                        "/swagger-ui/**",
+                        "/swagger-ui.html"
+                        ).permitAll()
+                        .requestMatchers(HttpMethod.GET, "/api/**")
+                        .hasAnyAuthority("SCOPE_ROLE_CLIENT", "SCOPE_ROLE_EMPLOYE", "SCOPE_ROLE_ADMIN")
+                        .requestMatchers(HttpMethod.POST, "/api/paiements/**")
+                        .hasAnyAuthority("SCOPE_ROLE_CLIENT", "SCOPE_ROLE_EMPLOYE", "SCOPE_ROLE_ADMIN")
+                        .requestMatchers(HttpMethod.POST, "/api/**")
+                        .hasAnyAuthority("SCOPE_ROLE_EMPLOYE", "SCOPE_ROLE_ADMIN")
+                        .requestMatchers(HttpMethod.PUT, "/api/**")
+                        .hasAnyAuthority("SCOPE_ROLE_EMPLOYE", "SCOPE_ROLE_ADMIN")
+                        .requestMatchers(HttpMethod.PATCH, "/api/**")
+                        .hasAnyAuthority("SCOPE_ROLE_EMPLOYE", "SCOPE_ROLE_ADMIN")
+                        .requestMatchers(HttpMethod.DELETE, "/api/**")
+                        .hasAuthority("SCOPE_ROLE_ADMIN")
+                        .anyRequest().authenticated())
+                .oauth2ResourceServer(oa->oa.jwt(Customizer.withDefaults()))
+                .build();
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource(){
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.addAllowedOrigin("*");
+        corsConfiguration.addAllowedMethod("*");
+        corsConfiguration.addAllowedHeader("*");
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfiguration);
+        return source;
+    }
+
+    @Bean
+    JwtEncoder jwtEncoder(){
+        return new NimbusJwtEncoder(new ImmutableSecret<>(secretKey.getBytes()));
+    }
+    @Bean
+    JwtDecoder jwtDecoder(){
+        SecretKeySpec secretKeySpec = new SecretKeySpec(secretKey.getBytes() , "HmacSHA512");
+        return NimbusJwtDecoder
+                .withSecretKey(secretKeySpec)
+                .macAlgorithm(MacAlgorithm.HS512)
+                .build();
+
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(UserDetailsService userDetailsService) {
+        DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
+        daoAuthenticationProvider.setPasswordEncoder(passwordEncoder());
+        daoAuthenticationProvider.setUserDetailsService(userDetailsService);
+        return new ProviderManager(daoAuthenticationProvider);
+    }
+
+}
